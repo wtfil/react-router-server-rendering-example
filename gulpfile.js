54397ca28fed5d1ae6e7f200fed8e69103527611
Filatov Evgen <evgen.filatov@gmail.com>
2015-06-24 20:27:17 +0300

    2

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var server = require('http-server');
+var fs = require('fs');
 
 var files = {
     js: {
@@ -11,10 +13,21 @@ var files = {
 gulp.task('js', function () {
     return browserify(files.js.src)
         .transform('reactify')
+        .on('error', function (e) {
+        	console.log(e);
+        })
         .bundle()
         .pipe(fs.createWriteStream(files.js.dest))
+});
+
+gulp.task('server', function (cb) {
+	var port = process.env.NODE_PORT || 3000;
+	server.createServer().listen(port, cb);
+	console.log('started at', port);
 });
 
 gulp.task('js-watch', function () {
     gulp.watch(files.js.src, ['js']);
 });
+
+gulp.task('dev', ['js-watch', 'server']);
